d66208b367ba515f0a517a292c037119c8c9c716
Marek Mierzwa <marek.mierzwa@brainly.com>
2020-11-20 22:06:04 +0100

    Tuned logging

---
+++ readme.md
@@ -0,0 +1,38 @@
+# Online duty API service
+
+## Configuration
+
+TODO
+
+## Development
+
+### Requirements
+
+- PHP 7.4+
+- Composer 2.0.0+
+
+### Local configuration
+
+After cloning the repository install packages:
+
+```commandline
+composer install
+```
+
+To run server locally:
+
+```commandline
+./script/server
+```
+
+To run tests:
+
+```commandline
+./script/test
+```
+
+To run code checks and dependencies security scan:
+
+```commandline
+./script/lint
+```
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use App\Service\ScheduleService;
 use DateTime;
+use Exception;
+use Monolog\Logger;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,13 +16,17 @@ class DefaultController extends AbstractController
 {
     private ScheduleService $service;
 
+    private LoggerInterface $log;
+
     /**
      * DefaultController constructor.
      * @param ScheduleService $service
+     * @param LoggerInterface $log
      */
-    public function __construct(ScheduleService $service)
+    public function __construct(ScheduleService $service, LoggerInterface $log)
     {
         $this->service = $service;
+        $this->log = $log;
     }
 
     /**
@@ -27,16 +34,24 @@ class DefaultController extends AbstractController
      */
     public function all(): JsonResponse
     {
-        $duties = $this->service->getAll();
-        $data = [];
+        try {
+            $duties = $this->service->getAll();
+            $data = [];
 
-        foreach ($duties as $duty) {
-            $data[] = [
-                'from' => $duty->getFrom()->format(DateTime::ISO8601),
-                'to' => $duty->getTo()->format(DateTime::ISO8601)
-            ];
+            foreach ($duties as $duty) {
+                $data[] = [
+                    'from' => $duty->getFrom()->format(DateTime::ISO8601),
+                    'to' => $duty->getTo()->format(DateTime::ISO8601)
+                ];
+            }
+
+            return new JsonResponse($data, Response::HTTP_OK);
+        } catch (Exception $ex) {
+            $this->log->error('cannot retrieve schedule', [
+                'errorMessage' => $ex->getMessage(),
+                'stack' => $ex->getTraceAsString()
+            ]);
+            return new JsonResponse('cannot retrieve online duties', Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-
-        return new JsonResponse($data, Response::HTTP_OK);
     }
 }
